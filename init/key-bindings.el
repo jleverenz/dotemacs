e3cc8f196636e1265f41d82be8fc4899105761da
Jeff Leverenz <jeff@dequeued.net>
2013-05-01 22:03:41 -0500

    Re-introduced often used key bindings.

--- init/key-bindings.el
+++ init/key-bindings.el
@@ -3,5 +3,9 @@
 (global-set-key [(control meta left)] 'swbuff-switch-to-next-buffer)
 (global-set-key [(control meta right)] 'swbuff-switch-to-previous-buffer)
 
+(global-set-key [(control meta up)] 'previous-error)
+(global-set-key [(control meta down)] 'next-error)
+
 (global-set-key [(control c)(down)] 'copy-line-down)
 (global-set-key [(control c)(s)] 'sort-lines)
+(global-set-key [(control meta return)] 'eval-and-replace)
