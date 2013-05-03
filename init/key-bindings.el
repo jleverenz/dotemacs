e3fad3caf62ce629cf4748024551bbe172543ac2
Jeff Leverenz <jeff@dequeued.net>
2013-05-02 20:50:08 -0500

    Bind M-/ to hippie-expand.

--- init/key-bindings.el
+++ init/key-bindings.el
@@ -5,6 +5,7 @@
 
 (global-set-key [(control meta up)] 'previous-error)
 (global-set-key [(control meta down)] 'next-error)
+(global-set-key [(meta /)] 'hippie-expand)
 
 (global-set-key [(control c)(down)] 'copy-line-down)
 (global-set-key [(control c)(s)] 'sort-lines)
