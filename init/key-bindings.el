f10766c1fd71238639594f4cc80b8870495cc75f
Jeff Leverenz <jeff.leverenz@gmail.com>
2013-06-06 22:25:18 -0500

    Added call-last-kbd-macro binding to C-"

--- init/key-bindings.el
+++ init/key-bindings.el
@@ -6,6 +6,7 @@
 (global-set-key [(control meta up)] 'previous-error)
 (global-set-key [(control meta down)] 'next-error)
 (global-set-key [(meta /)] 'hippie-expand)
+(global-set-key [(control \")] 'call-last-kbd-macro)
 
 (global-set-key [(control c)(down)] 'copy-line-down)
 (global-set-key [(control c)(s)] 'sort-lines)
