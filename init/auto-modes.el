9c87fcdd6f34571211f0903576f5992df065193e
Jeff Leverenz <jeff.leverenz@gmail.com>
2013-06-23 06:43:36 -0500

    Added new auto-modes file to set some favorite auto-mode-alist entries.

---
+++ init/auto-modes.el
@@ -0,0 +1,8 @@
+;; All my favorite auto-modes
+
+(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\Rakefile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\Rantfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("[Cc]apfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
