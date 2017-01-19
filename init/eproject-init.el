8a9707d89738bed0ba00d673ffedccaf3a0ccb1c
Jeff Leverenz <jeff.leverenz@gmail.com>
2017-01-19 00:54:38 -0600

    Remove anything references from eproject.

--- init/eproject-init.el
+++ init/eproject-init.el
@@ -5,8 +5,6 @@
 (require 'eproject-compile)
 (require 'eproject-selectors)
 (require 'eproject-find-cmd)
-(require 'anything)
-(require 'anything-config)
 (require 'grep)
 
 (define-project-type dotemacs (generic)
@@ -22,44 +20,3 @@
                        (concat (build-cd-and-find-cmd (eproject-root) find-cmd-sexp)
                               " -print0")
                        (smart-grep-args search-string)))))
-
-(defun anything-source-eproject--transform-to-short-names (candidates root)
-  (mapcar (lambda (candidate)
-            (let ((full-candidate-name (expand-file-name candidate)))
-              (cons (file-relative-name full-candidate-name root)
-                    full-candidate-name)))
-            candidates))
-
-(defvar anything-source-eproject-files
-  '((name . "Files in eproject")
-    (init . (lambda () (setq anything-source-eproject--root (eproject-root))))
-    (candidates . (lambda ()
-                    (eproject-list-project-files
-                     anything-source-eproject--root)))
-    (candidate-transformer . (lambda (candidates) (anything-source-eproject--transform-to-short-names
-                                                   candidates
-                                                   anything-source-eproject--root)))
-    (type . file)
-    (requires-pattern . 4)
-    (delayed)
-    ))
-
-(defun anything-selector ()
-  "Runs 'anything' with anything-sources temporarily set to
-whatever makes sense for the current buffer."
-  (interactive)
-  (cond (eproject-mode
-         (anything '(anything-c-source-buffers
-                     anything-source-eproject-files)))
-        (t
-         (anything '(anything-c-source-buffers)))))
-
-(add-hook 'eproject-mode-hook
-          (lambda ()
-            (define-key eproject-mode-map (kbd "C-,") 'eproject-find-cmd-custom-find-grep)
-            (define-key eproject-mode-map (kbd "C-.") 'anything-selector)
-            (define-key eproject-mode-map [(control return)]
-              '(lambda (arg) (interactive "P")
-                 (save-some-buffers t)
-                 (if arg (eproject-compile)
-                   (eproject-compile-repeat))))))
