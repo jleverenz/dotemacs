2393f1772eb1d095e252f574dc9e71e0e44000dc
Jeff Leverenz <jeff.leverenz@gmail.com>
2024-05-21 08:36:13 -0500

    Cleanup init.el

--- init.el
+++ init.el
@@ -15,12 +15,6 @@
 
 (add-to-list 'load-path "~/.emacs.d/init")
 (add-to-list 'load-path "~/.emacs.d/lisp")
-(mapcar 'load
-        '("utils"
-          "auto-complete-init"
-          "projectile-init"
-          "elpy-init"
-          "helm-init"))
 
 (add-hook 'after-init-hook (lambda () (load "after-init")))
 
@@ -31,15 +25,13 @@
  ;; If there is more than one, they won't work right.
  '(css-indent-offset 2)
  '(custom-safe-themes
-   (quote
-    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
+   '("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default))
  '(global-hl-line-mode t)
  '(helm-buffer-max-length nil)
  '(js-indent-level 2)
  '(line-spacing 0.25)
  '(package-selected-packages
-   (quote
-    (graphql-mode tuareg auto-complete dockerfile-mode typescript-mode php-mode exec-path-from-shell json-mode js-comint markdown-mode gitignore-mode rjsx-mode elpy ensime helm-projectile projectile ripgrep yafolding scala-mode yaml-mode helm)))
+   '(dotenv-mode graphql-mode tuareg auto-complete dockerfile-mode typescript-mode php-mode exec-path-from-shell json-mode js-comint markdown-mode gitignore-mode rjsx-mode elpy ensime helm-projectile projectile ripgrep yafolding scala-mode yaml-mode helm))
  '(typescript-indent-level 2))
 
 (custom-set-faces
@@ -53,3 +45,13 @@
  '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.6))))
  '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.3))))
  '(mode-line ((t (:foreground "white" :background "gray30")))))
+
+(package-refresh-contents)
+(package-install-selected-packages)
+
+(mapcar 'load
+        '("utils"
+          "auto-complete-init"
+          "projectile-init"
+          "elpy-init"
+          "helm-init"))
